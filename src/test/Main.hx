8919231b8c6475cb22b184aa2f5cbbabdb6cfc7c
Isaac Silverstein <logo4poop@protonmail.com>
2022-04-19 14:10:07 -0400

    More comprehensive test example, should have fixed gitmodules?

--- src/test/Main.hx
+++ src/test/Main.hx
@@ -6,11 +6,11 @@ import tink.http.Header.HeaderField;
 class Main {
 	static function main() {
 		var args = Sys.args();
-		trace(args[0]);
-		getUser(args[0], args[1]);
+		var user = loginPassword(args[0], args[1]);
+		trace(user.user_id);
 	}
 
-	static function loginPassword(username: String, password: String) {
+	static function loginPassword(username: String, password: String): User {
 		var body = '{
 			"type": "m.login.password",
 			"identifier": {
@@ -19,17 +19,38 @@ class Main {
 			},
 			"password": "$password"
 		}';
-		
+		var user: User = new User("", "", "", "");
+
 		tink.http.Client.fetch('https://matrix.org/_matrix/client/r0/login', {
 				method: POST,
-				headers: [new HeaderField(CONTENT_TYPE, 'application/json'), new HeaderField('content-length', Std.string(body.length))],
+				headers: [
+					new HeaderField(CONTENT_TYPE, 'application/json'), 
+					new HeaderField('content-length', Std.string(body.length))
+				],
 				body: body,
 				}).all()
 		.handle(function(o) switch o {
 			case Success(res):
-				trace(res.body);
+				var response_json = haxe.Json.parse(res.body.toString());
+				user = new User(response_json.user_id, response_json.access_token, response_json.home_server, response_json.device_id);
 			case Failure(e):
 				trace(e);
 		});
+
+		return user;
+	}
+}
+
+@:structInit class User {
+	public var user_id: String;
+	public var access_token: String;
+	public var home_server: String;
+	public var device_id: String;
+
+	public function new(user_id: String, access_token: String, home_server: String, device_id: String) {
+		this.user_id = user_id;
+		this.access_token = access_token;
+		this.home_server = home_server;
+		this.device_id = device_id;
 	}
 }
