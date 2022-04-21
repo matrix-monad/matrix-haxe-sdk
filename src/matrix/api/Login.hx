351f5d75eb09e867d0379ad045cc899d35db6de4
Isaac Silverstein <logo4poop@protonmail.com>
2022-04-21 12:08:06 -0400

    Create api package and started work on Login class within it

---
+++ src/matrix/api/Login.hx
@@ -0,0 +1,44 @@
+package matrix.api;
+
+import tink.http.Client.fetch;
+import tink.http.Header.HeaderField;
+import internal.Request;
+
+class Login {
+	typedef Login = {
+		var user_id:String;
+		var access_token:String;
+		var home_server:String;
+		var device_id:String;
+		// TODO: var well_known:WellKnown;
+	}
+
+	/*
+	typedef WellKnown = {
+	}
+	*/
+
+	static function withPassword(username:String, password:String):Login {
+		var body = {
+			type: "m.login.password",
+			identifier: {
+				type: "m.id.user",
+				user: '$username'
+			},
+			password: '$password'
+		};
+
+		var login:Login;
+
+		Request.post("/_matrix/client/r0/login", body, function (status, body, header) {
+			switch status {
+				case Success:
+					login = haxe.Json.parse(body.toString());
+				case Error:
+					trace(body);
+			}
+		});
+
+		return login;
+	}
+}
